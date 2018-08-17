d4baeb8ba69d50ae0651017505945ef4f8d62c53
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2018-08-17 15:54:08 +0200

    test/integration: Expect id conflicts to resolve as normal ones

--- test/integration/identity.js
+++ test/integration/identity.js
@@ -29,65 +29,157 @@ describe('Identity conflict', () => {
     await helpers.remote.ignorePreviousChanges()
   })
 
-  describe('initial detection', () => {
-    let idConflicts
-
+  context('with two remote dirs that cannot coexist locally', () => {
     beforeEach(async () => {
-      // FIXME: Extract event capture logic
-      idConflicts = []
-      helpers.events.on('id-conflict', idConflict => {
-        idConflicts.push(idConflict._id)
-      })
-
       await cozy.files.createDirectoryByPath('/alfred')
       await helpers.pullAndSyncAll()
 
       await cozy.files.createDirectoryByPath('/Alfred')
       await helpers.pullAndSyncAll()
-      should(await helpers.remote.tree()).deepEqual([
-        '.cozy_trash/',
-        'Alfred/',
-        'alfred/'
-      ])
     })
 
     onPlatforms('win32', 'darwin', () => {
-      it('syncs the first one but rejects the second one', async () => {
-        const actual = {
-          idConflicts,
+      it('syncs the first one and resolves the conflict on the second one', async () => {
+        should({
           incompatible: await helpers.incompatibleTree(),
-          local: await helpers.local.tree()
-        }
-        should(actual).deepEqual({
-          idConflicts: [
-            'ALFRED'
-          ],
+          local: await helpers.local.tree(),
+          remote: await helpers.remote.tree()
+        }).deepEqual({
           incompatible: [
             // Empty since none of them is incompatible by itself
           ],
           local: [
             'alfred/' // First one was synced
+          ],
+          remote: [
+            '.cozy_trash/',
+            'Alfred-conflict-.../',
+            'alfred/'
+          ]
+        })
+
+        // Sync lands on its feet on next remote polling
+        await helpers.pullAndSyncAll()
+        should({
+          incompatible: await helpers.incompatibleTree(),
+          local: await helpers.local.tree(),
+          remote: await helpers.remote.tree()
+        }).deepEqual({
+          incompatible: [],
+          local: [
+            'Alfred-conflict-.../',
+            'alfred/'
+          ],
+          remote: [
+            '.cozy_trash/',
+            'Alfred-conflict-.../',
+            'alfred/'
           ]
         })
       })
     })
   })
 
-  describe('remote resolution', () => {
-    let idConflicts
+  describe('unsynced local dir + remote dir', () => {
+    beforeEach(async () => {
+      await helpers.local.syncDir.ensureDir('alfred')
+      await helpers.local.scan()
+      await cozy.files.createDirectoryByPath('/Alfred')
+      await helpers.pullAndSyncAll()
+    })
 
+    onPlatforms('win32', 'darwin', () => {
+      it('renames the remote one', async () => {
+        should({
+          local: await helpers.local.tree(),
+          remote: await helpers.remote.tree()
+        }).deepEqual({
+          local: [
+            'alfred/'
+          ],
+          remote: [
+            '.cozy_trash/',
+            'Alfred-conflict-.../', // Remote one was renamed
+            'alfred/' // Local one was synced remotely
+          ]
+        })
+
+        // Sync lands on its feet on next remote polling
+        await helpers.pullAndSyncAll()
+        should({
+          incompatible: await helpers.incompatibleTree(),
+          local: await helpers.local.tree(),
+          remote: await helpers.remote.tree()
+        }).deepEqual({
+          incompatible: [],
+          local: [
+            'Alfred-conflict-.../',
+            'alfred/'
+          ],
+          remote: [
+            '.cozy_trash/',
+            'Alfred-conflict-.../',
+            'alfred/'
+          ]
+        })
+      })
+    })
+  })
+
+  describe('unsynced remote dir + local dir', () => {
+    beforeEach(async () => {
+      await cozy.files.createDirectoryByPath('/Alfred')
+      await helpers.remote.pullChanges()
+      await helpers.local.syncDir.ensureDir('alfred')
+      await helpers.local.scan()
+      await helpers.syncAll()
+    })
+
+    onPlatforms('win32', 'darwin', () => {
+      it('resolves the conflict on the remote dir, syncs the local one, then syncs the (renamed) remote one', async () => {
+        should({
+          local: await helpers.local.tree(),
+          remote: await helpers.remote.tree()
+        }).deepEqual({
+          local: [
+            'alfred/'
+          ],
+          remote: [
+            '.cozy_trash/',
+            'Alfred-conflict-.../', // Remote one was renamed
+            'alfred/' // Local one was synced remotely
+          ]
+        })
+
+        // Sync lands on its feet on next remote polling
+        await helpers.pullAndSyncAll()
+        should({
+          incompatible: await helpers.incompatibleTree(),
+          local: await helpers.local.tree(),
+          remote: await helpers.remote.tree()
+        }).deepEqual({
+          incompatible: [],
+          local: [
+            'Alfred-conflict-.../',
+            'alfred/'
+          ],
+          remote: [
+            '.cozy_trash/',
+            'Alfred-conflict-.../',
+            'alfred/'
+          ]
+        })
+      })
+    })
+  })
+
+  describe.skip('remote resolution', () => {
     beforeEach(async () => {
       await cozy.files.createDirectoryByPath('/alfred')
       await helpers.pullAndSyncAll()
 
       await cozy.files.createDirectoryByPath('/Alfred')
       await helpers.pullAndSyncAll()
-
-      // FIXME: Extract event capture logic
-      idConflicts = []
-      helpers.events.on('id-conflict', idConflict => {
-        idConflicts.push(idConflict._id)
-      })
 
       await cozy.files.updateAttributesByPath('/Alfred', {name: 'john'})
       await helpers.pullAndSyncAll()
@@ -95,13 +187,11 @@ describe('Identity conflict', () => {
 
     onPlatforms('win32', 'darwin', () => {
       it('works', async () => {
-        const actual = {
-          idConflicts,
+        should({
           incompatible: await helpers.incompatibleTree(),
           local: await helpers.local.tree(),
           remote: await helpers.remote.tree()
-        }
-        should(actual).deepEqual({
+        }).deepEqual({
           idConflicts: [],
           incompatible: [],
           local: [
