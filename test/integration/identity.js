8a159f283a71492cc31201ec4930f204a3357745
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2018-08-17 17:59:33 +0200

    test/integration: Also test identity conflicts on 2 files
    TODO: dir+file

--- test/integration/identity.js
+++ test/integration/identity.js
@@ -42,7 +42,7 @@ describe('Identity conflict', () => {
       itWorksAsExpected()
     })
 
-    describe('unsynced local dir + remote dir', () => {
+    describe('unsynced local + remote', () => {
       beforeEach(async () => {
         await helpers.local.syncDir.ensureDir('alfred')
         await helpers.local.scan()
@@ -53,7 +53,7 @@ describe('Identity conflict', () => {
       itWorksAsExpected()
     })
 
-    describe('unsynced remote dir + local dir', () => {
+    describe('unsynced remote + local', () => {
       beforeEach(async () => {
         await cozy.files.createDirectoryByPath('/Alfred')
         await helpers.remote.pullChanges()
@@ -114,6 +114,98 @@ describe('Identity conflict', () => {
               '.cozy_trash/',
               'Alfred-conflict-.../',
               'john/'
+            ]
+          })
+        })
+      })
+    }
+  })
+
+  describe('between two files', () => {
+    context('both remote', () => {
+      beforeEach(async () => {
+        await cozy.files.create('alfred content', {name: 'alfred'})
+        await helpers.pullAndSyncAll()
+
+        await cozy.files.create('Alfred content', {name: 'Alfred'})
+        await helpers.pullAndSyncAll()
+      })
+
+      itWorksAsExpected()
+    })
+
+    describe('unsynced local + remote', () => {
+      beforeEach(async () => {
+        await helpers.local.syncDir.outputFile('alfred', 'alfred content')
+        await helpers.local.scan()
+        await cozy.files.create('Alfred content', {name: 'Alfred'})
+        await helpers.pullAndSyncAll()
+      })
+
+      itWorksAsExpected()
+    })
+
+    describe('unsynced remote + local', () => {
+      beforeEach(async () => {
+        await cozy.files.create('Alfred content', {name: 'Alfred'})
+        await helpers.remote.pullChanges()
+        await helpers.local.syncDir.outputFile('alfred', 'alfred content')
+        await helpers.local.scan()
+        await helpers.syncAll()
+      })
+
+      itWorksAsExpected()
+    })
+
+    function itWorksAsExpected () {
+      onPlatforms('win32', 'darwin', () => {
+        it('resolves the conflict and syncs everything', async () => {
+          should({
+            local: await helpers.local.tree(),
+            remote: await helpers.remote.tree()
+          }).deepEqual({
+            local: [
+              'alfred'
+            ],
+            remote: [
+              '.cozy_trash/',
+              'Alfred-conflict-...',
+              'alfred'
+            ]
+          })
+
+          // Sync lands on its feet on next remote polling
+          await helpers.pullAndSyncAll()
+          should({
+            local: await helpers.local.tree(),
+            remote: await helpers.remote.tree()
+          }).deepEqual({
+            local: [
+              'alfred',
+              'Alfred-conflict-...'
+            ],
+            remote: [
+              '.cozy_trash/',
+              'Alfred-conflict-...',
+              'alfred'
+            ]
+          })
+
+          // Local <-> Remote relationships are not messed up
+          await cozy.files.updateAttributesByPath('/alfred', {name: 'john'})
+          await helpers.pullAndSyncAll()
+          should({
+            local: await helpers.local.tree(),
+            remote: await helpers.remote.tree()
+          }).deepEqual({
+            local: [
+              'Alfred-conflict-...',
+              'john'
+            ],
+            remote: [
+              '.cozy_trash/',
+              'Alfred-conflict-...',
+              'john'
             ]
           })
         })
